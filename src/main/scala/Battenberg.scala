fcd2731bff8e05b83a8175ebca0dd68242536612
martindruzbacky <druzbacky.martin@gmail.com>
2025-04-28 17:04:06 +0200

    subclones

--- src/main/scala/Battenberg.scala
+++ src/main/scala/Battenberg.scala
@@ -90,6 +90,34 @@ case class Battenberg(control_file: String, tumour_file: String):
       println(s"R script failed with exit code $exitCode")
     }
 
+    val call_sublocnes = Seq(
+      "Rscript",
+      "-e",
+      s"""
+              source("/app/ScalaBattenberg/src/main/R/fitCopyNumber.R")
+
+              callSubclones(
+                samplename = "${utils.tumourName}",
+                baf.segmented.file = "$input_baf_segment",
+                logr.file = "$logr_file",
+                rho.psi.file = "${utils.working_directory}/${utils.tumourName}_rho_and_psi.txt"
+                output.file = "${utils.working_directory}/${utils.tumourName}_copynumber.txt"
+                output.gw.figures.prefix=paste(samplename[sampleidx],"_BattenbergProfile", sep=""),
+                masking_output_file=paste(samplename[sampleidx], "_segment_masking_details.txt", sep=""),
+                chr_names= ${utils.chromosomeNames.toList}
+
+              )
+              """
+    )
+
+    exitCode = call_sublocnes.!
+
+    if (exitCode == 0) {
+      println("R script executed successfully")
+    } else {
+      println(s"R script failed with exit code $exitCode")
+    }
+
   }
 
   private def setDefaultValues(): Unit = {
