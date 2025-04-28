f024dfc4ed796bd04976fdb1524ce21df6bcafb1
martindruzbacky <druzbacky.martin@gmail.com>
2025-04-28 10:40:31 +0200

    Sorting fix

--- src/main/scala/Utils.scala
+++ src/main/scala/Utils.scala
@@ -432,7 +432,10 @@ case class Utils():
     }
     val extractChr = regexp_extract(col("Chromosome"), "^chr?(\\d+|X)", 1)
 
-    resultDf = resultDf.orderBy(when(extractChr === "X", 100).otherwise(extractChr.cast("int"), col("Position").cast("int")))
+    resultDf = resultDf.orderBy(
+      when(extractChr === "X", 100).otherwise(extractChr.cast("int")),
+      col("Position").cast("int")
+    )
     saveSingleFile(resultDf, s"${inputStart}heterozygousMutBAFs_haplotyped.txt")
   }
 
