20a02e5a9707326b1d028ed18b0e4e190bccb387
martindruzbacky <druzbacky.martin@gmail.com>
2025-05-01 21:32:37 +0200

    t

--- src/main/scala/Referencies.scala
+++ src/main/scala/Referencies.scala
@@ -9,13 +9,13 @@ class Referencies():
 
 
   def setPostfixDirectory(): Unit = {
-    g1000prefix = "/app/references38/1000G_loci_hg38_chr/1kg.phase3.v5a_GRCh38nounref_loci_"
-    g1000alleleprefix = "/Users/martindruzbacky/IdeaProjects/Battenberg/references38/1000G_loci_hg38_chr/1kg.phase3.v5a_GRCh38nounref_allele_index_"
-    impute_file = "/Users/martindruzbacky/IdeaProjects/Battenberg/references38/imputation_chr/impute_info.txt"
-    problemLociFile = "/Users/martindruzbacky/IdeaProjects/Battenberg/references38/probloci_chr/probloci.txt.gz"
-    beagleref = "/Users/martindruzbacky/IdeaProjects/Battenberg/references38/beagle_chr/CHROMNAME.1kg.phase3.v5a_GRCh38nounref.vcf.gz"
-    beaglejar = "/Users/martindruzbacky/IdeaProjects/Battenberg/references38/beagle_chr/beagle.08Feb22.fa4.jar"
-    beagleplink = "/Users/martindruzbacky/IdeaProjects/Battenberg/references38/beagle_chr/plink.CHROMNAME.GRCh38.map"
+    g1000prefix = "/app/references38/1000G_loci_hg38/1kg.phase3.v5a_GRCh38nounref_loci_"
+    g1000alleleprefix = "/app/references38/1000G_loci_hg38/1kg.phase3.v5a_GRCh38nounref_allele_index_"
+    impute_file = "/app/references38/impute_info.txt"
+    problemLociFile = "/app/references38/probloci/probloci.txt.gz"
+    beagleref = "/app/references38/beagle/CHROMNAME.1kg.phase3.v5a_GRCh38nounref.vcf.gz"
+    beaglejar = "/app/references38/beagle/beagle.08Feb22.fa4.jar"
+    beagleplink = "/app/references38/beagle/plink.CHROMNAME.GRCh38.map"
     //      beagleref = "/app/references38/beagle/chrCHROMNAME.1kg.phase3.v5a_GRCh38nounref.vcf.gz"
     //    ,
     //    beagleplink = "/app/references38/beagle/plink.chrCHROMNAME.GRCh38.map"
@@ -23,12 +23,12 @@ class Referencies():
   }
 
   def setPostFixFile(): Unit = {
-    beaglejar = "/Users/martindruzbacky/IdeaProjects/Battenberg/references38/beagle/beagle.08Feb22.fa4.jar"
-    beagleref = "/Users/martindruzbacky/IdeaProjects/Battenberg/references38/beagle/chrCHROMNAME.1kg.phase3.v5a_GRCh38nounref.vcf.gz"
-    beagleplink = "/Users/martindruzbacky/IdeaProjects/Battenberg/references38/beagle/plink.chrCHROMNAME.GRCh38.map"
+    beaglejar = "/app/references38/beagle/beagle.08Feb22.fa4.jar"
+    beagleref = "/app/references38/beagle/chrCHROMNAME.1kg.phase3.v5a_GRCh38nounref.vcf.gz"
+    beagleplink = "/app/references38/beagle/plink.chrCHROMNAME.GRCh38.map"
     g1000prefix = "/app/references38/1000G_loci_hg38/1kg.phase3.v5a_GRCh38nounref_loci_chr"
-    g1000alleleprefix = "/Users/martindruzbacky/IdeaProjects/Battenberg/references38/1000G_loci_hg38/1kg.phase3.v5a_GRCh38nounref_allele_index_chr"
-    impute_file = "/Users/martindruzbacky/IdeaProjects/Battenberg/references38/imputation/impute_info.txt"
-    problemLociFile = "/Users/martindruzbacky/IdeaProjects/Battenberg/references38/probloci/probloci.txt.gz"
+    g1000alleleprefix = "/app/references38/1000G_loci_hg38/1kg.phase3.v5a_GRCh38nounref_allele_index_chr"
+    impute_file = "/app/references38/imputation/impute_info.txt"
+    problemLociFile = "/app/references38/probloci/probloci.txt.gz"
   }
 
