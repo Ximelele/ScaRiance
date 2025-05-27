e473eeb10ca1d9c840b25a2ec8d0124b8b0cce3d
martindruzbacky <druzbacky.martin@gmail.com>
2025-05-27 13:11:21 +0200

    fix

--- src/main/scala/References.scala
+++ src/main/scala/References.scala
@@ -9,13 +9,13 @@ class References:
 
 
   def setPostfixDirectory(): Unit = {
-    g1000prefix = "/app/references38/1000G_loci_hg38/1kg.phase3.v5a_GRCh38nounref_loci_"
-    g1000alleleprefix = "/app/references38/1000G_loci_hg38/1kg.phase3.v5a_GRCh38nounref_allele_index_"
+    g1000prefix = "/app/references38/1000G_loci_hg38_chr/1kg.phase3.v5a_GRCh38nounref_loci_"
+    g1000alleleprefix = "/app/references38/1000G_loci_hg38_chr/1kg.phase3.v5a_GRCh38nounref_allele_index_"
     impute_file = "/app/references38/impute_info.txt"
-    problemLociFile = "/app/references38/probloci/probloci.txt.gz"
-    beagleref = "/app/references38/beagle/CHROMNAME.1kg.phase3.v5a_GRCh38nounref.vcf.gz"
-    beaglejar = "/app/references38/beagle/beagle.08Feb22.fa4.jar"
-    beagleplink = "/app/references38/beagle/plink.CHROMNAME.GRCh38.map"
+    problemLociFile = "/app/references38/probloci_chr/probloci.txt.gz"
+    beagleref = "/app/references38/beagle_chr/CHROMNAME.1kg.phase3.v5a_GRCh38nounref.vcf.gz"
+    beaglejar = "/app/references38/beagle_chr/beagle.08Feb22.fa4.jar"
+    beagleplink = "/app/references38/beagle_chr/plink.CHROMNAME.GRCh38.map"
     //      beagleref = "/app/references38/beagle/chrCHROMNAME.1kg.phase3.v5a_GRCh38nounref.vcf.gz"
     //    ,
     //    beagleplink = "/app/references38/beagle/plink.chrCHROMNAME.GRCh38.map"
